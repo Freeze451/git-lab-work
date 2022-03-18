10c3a135806240225ecb78a8359108f928a14639
Valentyn Veremeienko <veremeienko.valentyn@student.uzhnu.edu.ua>
2022-03-18 19:53:56 +0200

    [0004] Added terminate script, which runs after main function

--- index.ts
+++ index.ts
@@ -6,7 +6,12 @@ const main = () => {
   console.log('Hello world');
 };
 
+const terminate = () => {
+  console.log('-- After main --');
+};
+
 // Some fancy comment
 
 init();
 main();
+terminate();
